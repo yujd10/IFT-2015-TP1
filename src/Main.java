f1b48fae20a5fddc0d688ed5bace8543e682e60c
yujd10 <yujd10@outlook.com>
2021-10-03 18:30:21 -0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        CardHand<Card> cards= new CardHand<>();
+        CardHand cards= new CardHand();
         System.out.println(cards.ifEmpty());
         cards.addCard("2","Ace");
         System.out.println(cards.ifEmpty());
